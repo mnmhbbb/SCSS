e4bee27223667c9b3dd847c5235bca0cec3ce2e0
mnmhbbb <mnmhb64@gmail.com>
2021-07-06 23:12:57 +0900

    form&input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import avatarimg from './assets/images/img-user-01.jpg';
 import { ReactComponent as Star } from './assets/Icon/Star.svg';
+import { ReactComponent as Search } from './assets/Icon/Search.svg';
+import { ReactComponent as Caret } from './assets/Icon/Caret.svg';
 
 function App() {
   return (
@@ -53,6 +55,32 @@ function App() {
         <button type="button" disabled className="btn-ghost btn-32">
           버튼!
         </button>
+      </div>
+      <div>
+        <div className="select-group is-active">
+          <select className="form-select ">
+            <option value="1">
+              선택사항 1 Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Accusamus officiis, soluta molestias harum dolore odio
+              quasi. Asperiores, expedita. Architecto esse velit, labore
+              suscipit ipsum quas reprehenderit deserunt porro odio in ducimus,
+              maxime mollitia cCumque vitae in, recusandae eligendi delectus
+              beatae quod aperiam nihil dolore excepturi consequatur nulla,
+              culpa error? Nostrum possimus distinctio excepturi dignissimos
+              qui.
+            </option>
+            <option value="2">선택사항 2</option>
+            <option value="3">선택사항 3</option>
+            <option value="4">선택사항 4</option>
+            <option value="5">선택사항 5</option>
+          </select>
+          <Caret className="select-caret" />
+        </div>
+      </div>
+
+      <div className="input-group">
+        <Search className="input-search" />
+        <input type="text" className="form-input" placeholder="스토어 검색" />
       </div>
     </div>
   );
