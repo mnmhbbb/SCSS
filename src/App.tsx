8c6100fad557b6c0d1e607da8c0499f8bdd14a93
mnmhbbb <mnmhb64@gmail.com>
2021-07-06 18:09:49 +0900

    assets 폴더 위치 변경 & avatars img module

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import avatarimg from './assets/images/img-user-01.jpg';
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <strong className="tag-red">특가</strong>
-      <strong className="tag-green">무료배송</strong>
-      <strong className="tag-gray">첫 리뷰 두 배 적립</strong>
+      <div>
+        <strong className="tag-red">특가</strong>
+        <strong className="tag-green">무료배송</strong>
+        <strong className="tag-gray">첫 리뷰 두 배 적립</strong>
+      </div>
+      <div>
+        <div className="avatar-24">
+          <img src={avatarimg} alt="아바타 샘플" />
+        </div>
+        <div className="avatar-32">
+          <img src={avatarimg} alt="아바타 샘플" />
+        </div>
+      </div>
     </div>
   );
 }
