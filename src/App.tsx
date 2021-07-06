88c799ae9725275d86b207d9d702adeaed7aec8a
mnmhbbb <mnmhb64@gmail.com>
2021-07-06 14:02:56 +0900

    🎉 전체 세팅 완.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <strong className="tag-red">특가</strong>
+      <strong className="tag-green">무료배송</strong>
+      <strong className="tag-gray">첫 리뷰 두 배 적립</strong>
+    </div>
+  );
+}
+
+export default App;
